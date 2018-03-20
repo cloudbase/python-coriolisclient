87d3b5a5ad76934146c58cd4e6c8b2db97b576a3
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2018-03-20 12:12:15 +0200

    Add `endpoint destination options list` subcommand.

---
+++ coriolisclient/cli/endpoint_destination_options.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2017 Cloudbase Solutions Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+
+from cliff import lister
+from coriolisclient.cli import formatter
+
+
+class EndpointDestinationOptionFormatter(formatter.EntityFormatter):
+
+    columns = ("Option Name",
+               "Possible Values",
+               "Configuration Default")
+
+    def _get_sorted_list(self, obj_list):
+        return sorted(obj_list, key=lambda o: o.name)
+
+    def _get_formatted_data(self, obj):
+        data = (obj.name,
+                obj.values,
+                obj.to_dict().get("config_default"))
+        return data
+
+
+class ListEndpointDestinationOptions(lister.Lister):
+    """ List endpoint destination environment options. """
+
+    def get_parser(self, prog_name):
+        parser = super(ListEndpointDestinationOptions, self).get_parser(
+            prog_name)
+        parser.add_argument('endpoint', help='The endpoint\'s id')
+        parser.add_argument('--environment',
+                            help='JSON encoded endpoint environment data')
+        parser.add_argument(
+            '--options', help="Names of parameters to get options for.",
+            nargs='+', default=[])
+        return parser
+
+    def take_action(self, args):
+        if args.environment:
+            environment = json.loads(args.environment)
+        else:
+            environment = None
+
+        options = []
+        if args.options:
+            options = args.options
+
+        edo = self.app.client_manager.coriolis.endpoint_destination_options
+        obj_list = edo.list(
+            args.endpoint, environment=environment, option_names=options)
+        return EndpointDestinationOptionFormatter().list_objects(obj_list)
---
+++ coriolisclient/v1/endpoint_destination_options.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2018 Cloudbase Solutions Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import base64
+import json
+
+from coriolisclient import base
+
+
+class EndpointDestinationOption(base.Resource):
+    pass
+
+
+class EndpointDestinationOptionsManager(base.BaseManager):
+    resource_class = EndpointDestinationOption
+
+    def __init__(self, api):
+        super(EndpointDestinationOptionsManager, self).__init__(api)
+
+    def list(self, endpoint, environment=None, option_names=None):
+        url = '/endpoints/%s/destination-options' % base.getid(endpoint)
+
+        if environment:
+            encoded_env = base64.b64encode(
+                json.dumps(environment).encode()).decode()
+            url = '%s?env=%s' % (url, encoded_env)
+
+        if option_names:
+            sep = "?"
+            if environment:
+                sep = "&"
+            encoded_option_names = base64.b64encode(
+                json.dumps(option_names).encode()).decode()
+            url = '%s%soptions=%s' % (url, sep, encoded_option_names)
+
+        return self._list(url, 'destination_options')
