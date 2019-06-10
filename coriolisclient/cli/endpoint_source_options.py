1aceadeb19ebf85804c5d1a723c1492cc5a60515
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2019-06-10 15:55:39 +0300

    Add `endpoint source options list` command.

---
+++ coriolisclient/cli/endpoint_source_options.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2017 Cloudbase Solutions Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+
+from cliff import lister
+from coriolisclient.cli import formatter
+
+
+class EndpointSourceOptionFormatter(formatter.EntityFormatter):
+
+    columns = ("Option Name",
+               "Possible Values",
+               "Configuration Default")
+
+    def _get_sorted_list(self, obj_list):
+        return sorted(obj_list, key=lambda o: o.name)
+
+    def _get_formatted_data(self, obj):
+        data = (obj.name,
+                obj.values,
+                obj.to_dict().get("config_default"))
+        return data
+
+
+class ListEndpointSourceOptions(lister.Lister):
+    """ List endpoint source environment options. """
+
+    def get_parser(self, prog_name):
+        parser = super(ListEndpointSourceOptions, self).get_parser(
+            prog_name)
+        parser.add_argument('endpoint', help='The endpoint\'s id')
+        parser.add_argument('--environment',
+                            help='JSON encoded endpoint environment data')
+        parser.add_argument(
+            '--options', help="Names of parameters to get options for.",
+            nargs='+', default=[])
+        return parser
+
+    def take_action(self, args):
+        if args.environment:
+            environment = json.loads(args.environment)
+        else:
+            environment = None
+
+        options = []
+        if args.options:
+            options = args.options
+
+        endpoints = self.app.client_manager.coriolis.endpoints
+        endpoint_id = endpoints.get_endpoint_id_for_name(args.endpoint)
+        edo = self.app.client_manager.coriolis.endpoint_source_options
+        obj_list = edo.list(
+            endpoint_id, environment=environment, option_names=options)
+        return EndpointSourceOptionFormatter().list_objects(obj_list)
---
+++ coriolisclient/v1/endpoint_source_options.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2019 Cloudbase Solutions Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import base64
+import json
+
+from coriolisclient import base
+
+
+class EndpointSourceOption(base.Resource):
+    pass
+
+
+class EndpointSourceOptionsManager(base.BaseManager):
+    resource_class = EndpointSourceOption
+
+    def __init__(self, api):
+        super(EndpointSourceOptionsManager, self).__init__(api)
+
+    def list(self, endpoint, environment=None, option_names=None):
+        url = '/endpoints/%s/source-options' % base.getid(endpoint)
+
+        if environment:
+            encoded_env = base64.b64encode(
+                json.dumps(environment).encode()).decode()
+            url = '%s?env=%s' % (url, encoded_env)
+
+        if option_names:
+            sep = "?"
+            if environment:
+                sep = "&"
+            encoded_option_names = base64.b64encode(
+                json.dumps(option_names).encode()).decode()
+            url = '%s%soptions=%s' % (url, sep, encoded_option_names)
+
+        return self._list(url, 'source_options')
