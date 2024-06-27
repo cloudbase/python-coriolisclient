5be976971d5da9541c907d0f64c447739a7ca1ba
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-06-27 08:04:12 +0000

    tests: Adds mock patch autospec fixture
    When mocking something, there's the possibility to reference a
    property that does not exist, or call a method with invalid arguments,
    but the unit tests to keep passing, resulting in false positives.
    
    We've seen this issue several times in OpenStack, and we have created
    and started using a mock patch fixture (can be seen in OpenStack Nova's test.py)
    which enables autospec by default.
    
    This guarantees 2 things: that the thing we're patching exists, and if it's
    callable, that the calls respect the callable's signature (e.g.: no unknown
    arguments can be used).

--- coriolisclient/tests/test_base.py
+++ coriolisclient/tests/test_base.py
@@ -4,6 +4,13 @@
 """Defines base class for all tests."""
 
 from oslotest import base
+from oslotest import mock_fixture
+
+# NOTE(claudiub): this needs to be called before any mock.patch calls are
+# being done, and especially before any other test classes load. This fixes
+# the mock.patch autospec issue:
+# https://github.com/testing-cabal/mock/issues/396
+mock_fixture.patch_mock_module()
 
 
 class CoriolisBaseTestCase(base.BaseTestCase):
